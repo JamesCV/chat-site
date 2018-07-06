0a9f5c8c291f8400f759af9b9a44cb64627b087d
James Valentyne <Jvalentyne.63636@farnborough.ac.uk>
2018-07-06 23:27:20 +0100

    Commit

--- functions.js
+++ functions.js
@@ -1,12 +1,21 @@
 var fs = require(`fs`);
-var HTMLHeader = {'Content-Type': 'text/html'};
+var HTMLHeader = {"Content-Type": "Text/html"};
 
 var onRequest = function (req, res) {
-        res.writeHead(200, {"Content-Type": "Text/html"});
+        res.writeHead(200, HTMLHeader);
         fs.createReadStream("./index.html").pipe(res);    
     };
+
+var send404Request = function (req, res){
+    res.writeHead(404, HTMLHeader);
+    res.write("Page doesn't exist dumbass");
+}
+
+var sendRequest = function (req, res){
+    req.writeHead(200, HTMLHeader);
+    req.write(req);
+}
     
 
-      
-
+module.exports = send404Request;
 module.exports = onRequest;
