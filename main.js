214b4f82f48369638310e7dfb38daf75bc3b8af0
James Valentyne <JCvalentyne@11000th-of-my-assets.local>
2018-07-08 03:27:30 +0100

    commit

--- main.js
+++ main.js
@@ -2,13 +2,21 @@ var onRequest = require(`./functions`);
 var express = require('express');
 var app = express();
 var send404Request = require('./functions');
-
-if (app.get('/', onRequest)) {
-    app.listen(8888);
-} else {
-    send404Request();
-}
+var messages = [];
 
 
+app.get('/', onRequest);
+app.listen(8888);
+
+
+app.get('/postMessage', function(req, res) {
+    console.log(req.query)
+    messages.push(req.query);
+    res.end();
+  })
+
+app.get('/getMessage', function (req, res) {
+    res.end(JSON.stringify(messages));
+})
 
 console.log("Server is now running");
