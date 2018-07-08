79aa2f7f48a38fa2e71cad79c49114507b0581d5
James Valentyne <Jvalentyne.63636@farnborough.ac.uk>
2018-07-08 21:23:55 +0100

    commit

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
-var onRequest = require(`./functions`);
 var express = require('express');
 var app = express();
-var send404Request = require('./functions');
 var messages = [];
+var fs = require(`fs`);
+var messageArray = [];
 
 
 app.get('/', function (req, res) {
-    res.writeHead(200, HTMLHeader);
+    res.writeHead(200, {"Content-Type": "Text/html"});
     fs.createReadStream("./index.html").pipe(res);    
 })
 app.listen(8888);
 
 
 app.get('/postMessage', function(req, res) {
-    console.log(req.query)
     messages.push(req.query);
+    var counter = (messages.length - 1);
+    var newestMsg = getMsg(counter);
+    messageArray.push(newestMsg);
+    console.log(newestMsg);
     res.end();
   })
 
 app.get('/getMessage', function (req, res) {
-    res.end(JSON.stringify(messages));
+    res.end(JSON.stringify(messageArray));
 })
+
+
+
+var getMsg = function (item) {
+    var rawDate = messages[item];
+    var data = JSON.stringify(rawDate);
+    var curItem = data.split('"message":"')[1];
+    var dataItem = curItem.split('"}')[0];
+    return dataItem;
+}
 
 
 
